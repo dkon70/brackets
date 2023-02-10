fffb1cdf6bce2fd832ff1606ccf8dafc79f04d96
Dmitry Konev <dkon70@inbox.ru>
2023-02-11 02:25:39 +0700

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let pair = {};
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    pair[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (stack[stack.length - 1] === pair[str[i]]) {
+      stack.pop();
+    } else if (Object.values(pair).includes(str[i])) {
+      stack.push(str[i]);
+    }
+  }
+  return (stack.length === 0);
 }
