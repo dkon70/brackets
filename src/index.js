e2a576b5d1122b32a6b0190dcdf875ade090f8ac
Dmitry Konev <dkon70@inbox.ru>
2023-02-11 03:54:15 +0700

    fix: errors fixed

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ module.exports = function check(str, bracketsConfig) {
   let stack = [];
   let pair = {};
   for (let i = 0; i < bracketsConfig.length; i++) {
-    pair[bracketsConfig[i][1]] = bracketsConfig[i][0];
+    pair[bracketsConfig[i][0]] = bracketsConfig[i][1];
   }
   for (let i = 0; i < str.length; i++) {
-    if (stack[stack.length - 1] === pair[str[i]]) {
+    if (str[i] === pair[stack[stack.length - 1]]) {
       stack.pop();
-    } else if (Object.values(pair).includes(str[i])) {
+    } else if (Object.keys(pair).includes(str[i])) {
       stack.push(str[i]);
     }
   }
-  return (stack.length === 0);
+  return(stack.length === 0);
 }
